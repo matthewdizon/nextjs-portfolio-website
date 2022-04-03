2956c46dfeeff4f1659993b44e821dfdef88af32
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-04-03 21:55:46 +0800

    project side border and tech stack

--- pages/projects.js
+++ pages/projects.js
@@ -15,15 +15,30 @@ const ProjectsContainer = styled.div`
 const Project = styled.div`
     display: flex;
     flex: 1 1 0;
+    border-left: 4px solid black;
 
-    padding: 16px 0;
+    margin-bottom: 48px;
     /* justify-content: space-evenly; */
 
     :nth-child(even) {
         flex-direction: row-reverse;
+        border-right: 4px solid black;
+        border-left: none;
 
         .project-details {
             text-align: right;
+        }
+
+        .image-container {
+            margin: 0 20px 0 0;
+        }
+
+        .tech-container {
+            justify-content: flex-end;
+
+            .tech {
+                margin: 0 0 0 8px !important;
+            }
         }
     }
 
@@ -36,8 +51,14 @@ const Project = styled.div`
         -moz-box-shadow: -3px 2px 23px 3px rgba(0,0,0,0.75);
         box-shadow: -3px 2px 23px 3px rgba(0,0,0,0.75);
 
+        margin: 0 0 0 20px;
+
         transition-duration: 0.3s;
         transition-property: transform;
+
+        img {
+            border-radius: 8px;
+        }
 
         :hover {
             transform: scale(1.05);
@@ -60,6 +81,21 @@ const Project = styled.div`
         button {
             :hover {
                 cursor: pointer;
+            }
+        }
+
+        .tech-container {
+            display: flex;
+            padding-bottom: 16px;
+            
+            .tech {
+                font-size: 12px;
+                padding: 2px 16px;
+                margin: 0 8px 0 0;
+                border: 0.5px solid black;
+                border-radius: 20px;
+
+                background-color: aliceblue;
             }
         }
     }
@@ -85,7 +121,8 @@ export default function Projects() {
                 <Hero
                     title="🛠 Projects"
                     description="Check out all of the projects I've made contributions to throughout my career as a software developer!"
-                />                    
+                />
+                <h2 style={{textAlign: "center", fontSize: "36px"}}>🌐 Websites</h2>
                 <ProjectsContainer>
                     {
                         projects.map(project => {
@@ -93,11 +130,23 @@ export default function Projects() {
                                 <Project>
                                     <div className="image-container">
                                         {/* <Image src={project.image} alt={project.title} width="1000" height="500" objectFit='cover'/> */}
-                                        <Image src={project.image} alt={project.title} layout="fill" objectFit='cover' />
+                                        <a href={project.link} target="_blank">
+                                            <Image src={project.image} alt={project.title} layout="fill" objectFit='cover' />
+                                        </a>
                                     </div>
                                     <div className="project-details">
                                         <h1>{project.title}</h1>
                                         <p>{project.description}</p>
+                                        <div className="tech-container">
+                                            {project.tech.map(tech => {
+                                                return (
+                                                    <div className="tech">
+                                                        {/* <p>{tech}</p> */}
+                                                        {tech}
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
                                         <a href={project.link} target="_blank"><button>View Project</button></a>
                                     </div>
                                 </Project>
