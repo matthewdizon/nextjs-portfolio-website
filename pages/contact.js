047fec2a447d453d0083a14ad72226042e62d409
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-04-06 03:03:09 +0800

    added cards for contact and experience pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,38 @@
 import Hero from "../components/Hero"
 import Layout from "../components/Layout"
+import styled from 'styled-components'
+
+const ContactContainer = styled.div`
+    
+    padding: 48px 96px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+
+    .contact {
+        display: inline-block;
+        text-align: center;
+        
+        border-radius: 10px;
+        padding: 24px 60px;
+        margin-bottom: 16px;
+        
+        box-shadow: rgba(0, 0, 0, 0.1) 4px 4px 4px 4px;
+        /* box-shadow: 0 4px 10px var(black); */
+
+        transition-duration: 0.3s;
+        transition-property: transform;
+
+        span {
+            font-size: 48px;
+        }
+
+        :hover {
+            cursor: pointer;
+            transform: translateY(-3px);
+        }
+    }
+`
 
 export default function Contact() {
     return (
@@ -8,20 +41,28 @@ export default function Contact() {
                 title="📞 Contact"
                 description="Feel free to connect with me to discuss software development and business opportunities. I am also available for freelance and internship opportunities."
             />
-            <div>
-                <div>
-                    <p>📧</p>
+            <ContactContainer>
+                <div className="contact">
+                    <span>📧</span>
                     <h2>Email</h2>
+                    <p>Send me an email at dominic.dizon@obf.ateneo.edu</p>
                 </div>
-                <div>
-                    <p>📧</p>
+                <div className="contact">
+                    <span>📧</span>
                     <h2>Email</h2>
+                    <p>Send me an email at dominic.dizon@obf.ateneo.edu</p>
                 </div>
-                <div>
-                    <p>📧</p>
+                <div className="contact">
+                    <span>📧</span>
                     <h2>Email</h2>
+                    <p>Send me an email at dominic.dizon@obf.ateneo.edu</p>
                 </div>
-            </div>
+                <div className="contact">
+                    <span>📧</span>
+                    <h2>Email</h2>
+                    <p>Send me an email at dominic.dizon@obf.ateneo.edu</p>
+                </div>
+            </ContactContainer>
         </Layout>
     )
 }
--- pages/experience.js
+++ pages/experience.js
@@ -1,5 +1,58 @@
 import Hero from "../components/Hero"
 import Layout from "../components/Layout"
+import styled from 'styled-components'
+import Image from 'next/image'
+
+const ExperienceContainer = styled.div`
+
+    padding: 48px 96px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    text-align: center;
+
+    .experience {
+        padding: 48px 0;
+        margin: 0 24px 24px 0;
+        width: 80%;
+
+        display: flex;
+        justify-content: space-evenly;
+        position: relative;
+        
+        align-items: center;
+
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.1) 4px 4px 4px 4px;
+
+        .date {
+            font-size: 14px;
+            font-style: italic;
+            font-weight: 500;
+
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            padding: 0 16px 16px 0;
+        }
+
+        .image-container {
+            position: relative;
+            width: 40%;
+            
+            img {
+                border: 1px solid black !important;
+                border-radius: 50%;
+            }
+        }
+        
+        .info-container {
+            margin-right: 64px;
+            text-align: left;
+            width: 60%;
+        }
+    }
+`
 
 export default function Experience() {
     return (
@@ -8,26 +61,38 @@ export default function Experience() {
                 title="🤝 Experience"
                 description="Check out the different organizations and respective positions I've taken!"
             />
-            <div>
-                <div>
-                    <span>Year</span>
-                    <h2>Position</h2>
-                    <h3>Organization</h3>
-                    <p>Description</p>
+            <ExperienceContainer>
+                <div className="experience">
+                    <div className="image-container">
+                        <Image src="/atomic-habits.jpg" height={200} width={200} objectFit="cover" />
+                    </div>
+                    <div className="info-container">
+                        <h2>Senior Software Engineer E awoeijf aoiwjef oaiwjef</h2>
+                        <h3>Organization</h3>
+                    </div>
+                    <span className="date">June '20 - January '21</span>
                 </div>
-                <div>
-                    <span>Year</span>
-                    <h2>Position</h2>
-                    <h3>Organization</h3>
-                    <p>Description</p>
+                <div className="experience">
+                    <div className="image-container">
+                        <Image src="/atomic-habits.jpg" height={200} width={200} objectFit="cover" />
+                    </div>
+                    <div className="info-container">
+                        <h2>Senior Software Engineer E awoeijf aoiwjef oaiwjef</h2>
+                        <h3>Organization</h3>
+                    </div>
+                    <span className="date">June '20 - January '21</span>
                 </div>
-                <div>
-                    <span>Year</span>
-                    <h2>Position</h2>
-                    <h3>Organization</h3>
-                    <p>Description</p>
+                <div className="experience">
+                    <div className="image-container">
+                        <Image src="/atomic-habits.jpg" height={200} width={200} objectFit="cover" />
+                    </div>
+                    <div className="info-container">
+                        <h2>Senior Software Engineer E awoeijf aoiwjef oaiwjef</h2>
+                        <h3>Organization</h3>
+                    </div>
+                    <span className="date">June '20 - January '21</span>
                 </div>
-            </div>
+            </ExperienceContainer>
         </Layout>
     )
 }
